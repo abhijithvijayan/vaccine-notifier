50fce877004662de22e4341f01559264b4441e17
abhijithvijayan <34790378+abhijithvijayan@users.noreply.github.com>
2021-06-05 15:40:32 +0530

    feat: get ntp date time after wifi initialization

--- vaccine-notifier-esp8266.ino
+++ vaccine-notifier-esp8266.ino
@@ -6,11 +6,14 @@
  *  @title:   Vaccine Notifier for COWIN WebApp
  *
  */
-#include <Arduino_JSON.h>    // https://github.com/bblanchon/ArduinoJson
+#include <Arduino_JSON.h> // https://github.com/bblanchon/ArduinoJson
 #include <ESP8266WiFi.h>
+#include <NTPClient.h> // https://github.com/arduino-libraries/NTPClient
 #include <Ticker.h>
+#include <WiFiUdp.h>
 
-#define PING_INTERVAL 60000 // 1minute
+#define PING_INTERVAL 10000                                  // 1minute
+const long utcOffsetInSeconds = ((5 * 60 * 60) + (30 * 60)); // UTC+5:30
 
 // Network
 #define WIFI_SSID "........"
@@ -22,6 +25,39 @@
 WiFiEventHandler wifiConnectHandler;
 WiFiEventHandler wifiDisconnectHandler;
 Ticker wifiReconnectTimer;
+
+// Define NTP Client to get time
+WiFiUDP ntpUDP;
+NTPClient timeClient(ntpUDP, "pool.ntp.org", utcOffsetInSeconds);
+
+long lastExecutionTime = 0;
+
+unsigned long getTime() {
+    timeClient.update();
+
+    return timeClient.getEpochTime();
+}
+
+String getCurrentDate() {
+    unsigned long epochTime = getTime();
+    // Get a time structure
+    /**
+     *  Get a time structure
+     *  http://www.cplusplus.com/reference/ctime/tm/
+     *
+     *  tm_sec: seconds after the minute
+     *  tm_min: minutes after the hour
+     *  tm_hour: hours since midnight
+     *  tm_mday: day of the month
+     *  tm_year: years since 1900
+     */
+    struct tm *ptm = gmtime((time_t *)&epochTime);
+    int day        = ptm->tm_mday;
+    int month      = ptm->tm_mon + 1;
+    int year       = ptm->tm_year + 1900;
+
+    return String(day) + '-' + String(month) + '-' + String(year);
+}
 
 /**
  *  Handle WiFi Connectivity
@@ -44,6 +80,7 @@ void onWifiConnect(const WiFiEventStationModeGotIP &event) {
     Serial.println(WiFi.localIP());
 
     // Get internet time
+    timeClient.begin();
 }
 
 /**
@@ -54,7 +91,6 @@ void onWifiDisconnect(const WiFiEventStationModeDisconnected &event) {
 
     wifiReconnectTimer.once(2, connectToWifi);
 }
-
 
 void setup() {
     pinMode(BUILTIN_LED, OUTPUT); // Initialize the BUILTIN_LED pin as an output
@@ -69,13 +105,11 @@ void setup() {
     connectToWifi();
 }
 
-long lastExecutionTime = 0;
-
 void loop() {
-  long now = millis();
+    long now = millis();
 
-  if (now - lastExecutionTime > PING_INTERVAL) {
-      Serial.println("Hello World");
-  }
-
+    if (now - lastExecutionTime > PING_INTERVAL) {
+        lastExecutionTime = now;
+        Serial.println(getCurrentDate());
+    }
 }
