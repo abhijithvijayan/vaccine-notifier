a45f726f32fa1f69f85974ef83e91deba04619b8
abhijithvijayan <34790378+abhijithvijayan@users.noreply.github.com>
2021-06-06 23:09:04 +0530

    feat: lambda function for triggering notifications

--- notifier-lambda/api/notifier.js
+++ notifier-lambda/api/notifier.js
@@ -1,3 +1,12 @@
+const {get} = require('@abhijithvijayan/ts-utils');
+const fetch = require('node-fetch');
+
+const {
+  TELEGRAM_CHAT_ID = '',
+  TELEGRAM_BOT_TOKEN = '',
+  SLACK_WEBHOOK_URL = '',
+} = process.env;
+
 function adjustForTimezone(date, offset = 0) {
   const timeOffsetInMS = offset * 60 * 60 * 1000;
   date.setTime(date.getTime() + timeOffsetInMS);
@@ -18,5 +27,60 @@ module.exports.getCurrentDate = async () => {
   return {
     statusCode: 200,
     body: currentDate(),
+  };
+};
+
+module.exports.notifyIfAvailable = async () => {
+  const slots = [];
+
+  const telegramURL = `https://api.telegram.org/bot${TELEGRAM_BOT_TOKEN}/sendMessage`;
+  const body = ({format = false} = {}) => {
+    return `
+          Timestamp: ${new Intl.DateTimeFormat('en-GB', {
+            dateStyle: 'full',
+            timeStyle: 'long',
+          }).format(
+            adjustForTimezone(new Date(), 5.5)
+          )},\n\nAvailable Centers:  ${JSON.stringify(
+      slots,
+      null,
+      format ? 2 : 0
+    )}`;
+  };
+
+  const [tgResponse, slackResponse] = await Promise.allSettled([
+    fetch(telegramURL, {
+      method: 'POST',
+      body: JSON.stringify({
+        chat_id: TELEGRAM_CHAT_ID,
+        text: body({format: true}),
+      }),
+      headers: {'Content-Type': 'application/json'},
+    }),
+    fetch(SLACK_WEBHOOK_URL, {
+      method: 'post',
+      body: JSON.stringify({
+        text: body({format: false}),
+      }),
+      headers: {'Content-Type': 'application/json'},
+    }),
+  ]);
+
+  if (
+    tgResponse.status === 'fulfilled' ||
+    slackResponse.status === 'fulfilled'
+  ) {
+    if (
+      get(tgResponse, 'value.ok', false) ||
+      get(slackResponse, 'value.ok', false)
+    ) {
+      console.log('[SUCCESS]: Notification sent.');
+    }
+  } else {
+    console.log('[ERROR]: Failed to send notification');
+  }
+
+  return {
+    statusCode: 200,
   };
 };
